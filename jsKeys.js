2e80fa83062538febb13f55e53821ce23154f5ba
Weston <classicmclive@gmail.com>
2024-12-25 23:56:55 -0500

    Update jsKeys.js

--- jsKeys.js
+++ jsKeys.js
@@ -5,15 +5,19 @@ let downKeyPressed = false;
 let key = "none";
 function keyPressed(evt){
   if(evt.keyCode == 37||evt.keyCode==65){
+      key = "pressed"
       leftKeyPressed = true;
   }
   else if(evt.keyCode == 39||evt.keyCode==68){
+      key = "pressed"
       rightKeyPressed = true;
   }
   else if(evt.keyCode == 38||evt.keyCode==87){
+      key = "pressed"
       upKeyPressed = true;
   }
   else if(evt.keyCode == 40||evt.keyCode==83){
+      key = "pressed"
       downKeyPressed = true;
   }
 }
